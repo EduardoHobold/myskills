7896dea6d0d2b99a637cabd9bc55c4754d0473ec
Eduardo Hobold <hobold.edu@hotmail.com>
2022-09-08 12:59:54 -0300

    Salvando projeto

---
+++ src/components/SkillCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {
+    TouchableOpacity,
+    TouchableOpacityProps,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+interface SkillCardProps extends TouchableOpacityProps {
+    skill: string;
+}
+export function SkillCard({ skill, ...rest }: SkillCardProps) {
+    return (
+        <TouchableOpacity 
+            style={styles.buttonSkill}
+            {...rest}
+        >
+            <Text style={styles.skill}>
+                {skill}
+            </Text>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    buttonSkill: {
+        backgroundColor: '#1F1e25',
+        padding: 15,
+        borderRadius: 50,
+        alignItems: 'center',
+        marginVertical: 10
+    },
+    skill: {
+        color: '#FFF',
+        fontSize: 22,
+        fontWeight: 'bold',
+    }
+})
